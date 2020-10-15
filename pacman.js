7b26205c3eb244be30150363ad8079f90fa9f24d
Albin <Albin@dahlen.ws>
2020-10-15 21:55:54 +0200

    Börjat fixa väggkollision

--- pacman.js
+++ pacman.js
@@ -1,28 +1,93 @@
 class Pacman {
-  constructor() {
+  constructor(maze) {
     this.x = width / 2;
-    this.y = height * 0.738;
+    this.y = 15 + 15 * scl;
     this.direction = {
-      xdir: 0,
-      ydir: 0,
+      x: 0,
+      y: 0,
     };
     this.speed = 3;
+    this.maze = maze;
   }
 
   show() {
     fill(255, 255, 0);
-    ellipse(this.x, this.y, 25);
+    ellipse(this.x, this.y, 24);
   }
 
   update() {
-    this.x += this.direction.xdir * this.speed;
-    this.y += this.direction.ydir * this.speed;
+    // this.wallcollision(this.direction.x, this.direction.y);
+    this.handleMovement(this.direction.x, this.direction.y);
+    this.x += this.direction.x * this.speed;
+    this.y += this.direction.y * this.speed;
+  }
+
+  handleMovement(xdir, ydir) {
+    /*TODO
+      Kolla om nästa steg resulterar i krock med vägg.
+      Om man rör sig i x-led och vill byta till y-led,
+      men y-led leder till krock med vägg = fortsätt i
+      samma x-led riktning.
+    */
+    if (xdir != 0) {
+      if (this.wallcollision(xdir, ydir)) {
+        this.setdir(0, this.direction.y);
+      } else {
+        this.setdir(xdir, ydir);
+      }
+    } else if (ydir != 0) {
+      if (this.wallcollision(xdir, ydir)) {
+        this.setdir(this.direction.x, 0);
+      } else {
+        this.setdir(xdir, ydir);
+      }
+    }
+  }
+
+  test(xdir, ydir) {
+    if (this.wallcollision(xdir, ydir)) {
+      if (this.direction.x != 0) {
+        this.setdir(this.direction.x, 0);
+      } else if (this.direction.y != 0) {
+        this.setdir(0, this.direction.y);
+      } else {
+        this.setdir(0, 0);
+      }
+      this.setdir(0, 0);
+    } else {
+      this.setdir(xdir, ydir);
+    }
   }
 
   setdir(xdir, ydir) {
-    this.direction.xdir = xdir;
-    this.direction.ydir = ydir;
+    this.direction.x = xdir;
+    this.direction.y = ydir;
   }
 
-  wallcollision() {}
+  contains(x, y) {
+    if (this.maze.grid[floor(y / scl)][floor(x / scl)] == 0) {
+      console.log("Wall detected");
+      return true;
+    }
+    return false;
+  }
+
+  wallcollision(x, y) {
+    var newX = this.x + x * this.speed;
+    var newY = this.y + y * this.speed;
+
+    if (x > 0 || y > 0) {
+      newX += 12;
+      newY += 12;
+    } else if (x < 0 || y < 0) {
+      newX -= 13;
+      newY -= 13;
+    }
+
+    if (this.contains(newX, newY)) {
+      // this.setdir(0, 0);
+      return true;
+    }
+    return false;
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,22 +1,38 @@
-let map, pacman;
+let maze, pacman;
 let scl = 30;
 
 function setup() {
-  createCanvas(17 * scl, 21 * scl);
-  map = new Map();
-  pacman = new Pacman();
+  createCanvas(19 * scl, 21 * scl);
+  maze = new Maze();
+  pacman = new Pacman(maze);
 }
 
 function draw() {
   background(0);
-  map.show();
-  pacman.show();
+  maze.show();
   pacman.update();
+  // pacman.move();
+  pacman.show();
 }
 
-function keyTyped() {
-  if (key === "a") pacman.setdir(-1, 0);
-  else if (key === "d") pacman.setdir(1, 0);
-  else if (key === "w") pacman.setdir(0, -1);
-  else if (key === "s") pacman.setdir(0, 1);
+function keyPressed() {
+  if (key === "a") pacman.handleMovement(-1, 0);
+  else if (key === "d") pacman.handleMovement(1, 0);
+  else if (key === "w") pacman.handleMovement(0, -1);
+  else if (key === "s") pacman.handleMovement(0, 1);
+  // if (key === "a") pacman.setdir(-1, 0);
+  // else if (key === "d") pacman.setdir(1, 0);
+  // else if (key === "w") pacman.setdir(0, -1);
+  // else if (key === "s") pacman.setdir(0, 1);
+  // if (key === "a") pacman.move(-30, 0);
+  // else if (key === "d") pacman.move(30, 0);
+  // else if (key === "w") pacman.move(0, -30);
+  // else if (key === "s") pacman.move(0, 30);
 }
+
+// function keyReleased() {
+//   if (key === "a") pacman.setdir(0, 0);
+//   else if (key === "d") pacman.setdir(0, 0);
+//   else if (key === "w") pacman.setdir(0, 0);
+//   else if (key === "s") pacman.setdir(0, 0);
+// }
